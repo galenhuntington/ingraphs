312935699bff5d72589f032ca2cb98622a2e192c
Galen Huntington <galen.huntington@gmail.com>
2026-08-20 00:49:15 -0700

    Add proof for 14 and for 9X.

--- proofs/9.sh
+++ proofs/9.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 graphy ingraph-scan 9 <(graphy canon 9 <(geng -q 9 8:9)) | grep None
 # ~1s -> 4 graphs
+geng -q 9 > output/all9.txt && graphy misses 9 101752
+# -> only one counterexample for 9X
+graphy is-subgraph 101752 / 4135458444 "$(graphy complement 9 4135458444)"
+# = false false
