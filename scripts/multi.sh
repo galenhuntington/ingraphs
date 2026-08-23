4c41b0f0682193b3009eceb858d8bdb6f0f86020
Galen Huntington <galen.huntington@gmail.com>
2026-08-23 11:12:37 -0700

    multi.sh: fix seeds extension, more logging

--- scripts/multi.sh
+++ scripts/multi.sh
@@ -5,8 +5,12 @@ MOD="$2"
 BATCH="$3"
 
 # 14:
-#   105760086734720 and 105760105576384 are very slow so usually separate
+#   105760086734720 and 105760105576384 are very slow (symmetric) so usually separate
 #   recently refuted to use as controls: 105760631863104, 35391896139584, 207515662272, 105760095090496
+#   last is hard, many runs without re-refutation
+# Example:
+#   time seq 1 18 | xargs -P6 -I{} ./scripts/multi.sh 14 5000000 std14 {}
+# (final {} is unused but tracks progress in process command line)
 
 # shellcheck disable=SC2046 disable=SC2005
 GRAPHS=$(echo $(cat "output/batches/$BATCH"))
@@ -23,7 +27,8 @@ RES=$(( SRANDOM % MOD ))
 LOG="$DIR/log-$RES.log"
 MID=$(( (SIZE*(SIZE-1)+3)/4 ))
 
-SEEDS=$DIR/seeds-$RES.csv
+# XXX maybe each RES a directory?
+SEEDS=$DIR/seeds-$RES.g6
 FOUND=$DIR/found-$RES.csv
 
 if [ -f "$FOUND" ]; then
@@ -36,6 +41,7 @@ fi
     time GRAPHY_SUB="${GRAPHS//\ /,}" ./nauty-prune/gengf -q -X1 "$SIZE" $MID:$MID "$RES/$MOD" >"$SEEDS"
     : > "$FOUND"
     for g in $GRAPHS; do
+       echo "# $g" >&2
        {
          echo "# $g"
          ./target/release/graphy free-close "$SIZE" "$g" <(./target/release/graphy free-scan "$SIZE" "$g" "$SEEDS") | grep false
